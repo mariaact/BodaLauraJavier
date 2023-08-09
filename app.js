b6242fe6f0a23d7f76928ecf91bf770bde0a2962
mariaact <mariactyermo@gmail.com>
2023-08-09 20:23:42 +0200

    conectar bbdd con formulario

--- app.js
+++ app.js
@@ -3,11 +3,54 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+/*-----------------------*/
+const session = require('express-session');
+app.use(session({
+  secret: 'tu_secreto_aqui',
+  resave: false,
+  saveUninitialized: true
+}));
+
+const { exec } = require('child_process'); 
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+  const success = req.query.success === 'true';
+  res.render('index', { success });
+
+ // res.render('index');
+});
+
+app.post('/submit', (req, res) => {
+  const { nombre, acompanado, nombreAcompanado, plazaBus, cantidadPlazaBus, alergia, alergiaAlimento, Menu, tipoMenu } = req.body;
+
+  const phpScript = `php public/php/insert.php "${nombre}" "${acompanado}" "${nombreAcompanado}" "${plazaBus}" "${cantidadPlazaBus}" "${alergia}" "${alergiaAlimento}" "${Menu}" "${tipoMenu}"`;
+
+ // const phpScript = `"C:\\xampp\\\install\\php.exe" ${path.join(__dirname, 'public/php/insert.php')} "${nombre}" "${acompanado}" "${nombreA}" "${acompanadoNinos}" "${cantidadninos}" "${plazaBus}" "${cantidadPlazaBus}" "${alergia}" "${alergiaAlimento}" "${Menu}" "${tipoMenu}"`;
+  exec(phpScript, (error, stdout, stderr) => {
+    if (error) {
+      console.error('Error al ejecutar el script PHP:', error);
+      res.status(500).send('Error al enviar el formulario');
+    } else {
+      console.log('Registro insertado correctamente');
+      res.redirect('/?success=true');
+    }
+  });
+});
+
+
+
+/*-----------------------*/
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
